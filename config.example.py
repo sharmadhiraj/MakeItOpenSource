f139cb439b71e8cc067b8dca8b7b8db87e9e3dc9
Dhiraj Sharma <sharmadhiraj.np@gmail.com>
2019-03-01 13:17:25 +0545

    Create repo & get clone_url from response.

---
+++ config.example.py
@@ -0,0 +1,1 @@
+access_token = "your_personal_access_token"
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import requests
+
+from config import access_token
 from utils import show_error_and_exit
 
 print("Welcome to Make It Open Source")
@@ -10,18 +12,21 @@ while not repo_name:
 repo_description = input('Enter repository description(optional) : ')
 
 repo_info = {'name': repo_name, 'description': repo_description}
-url = "https://api.github.com/users/repos?access_token=<generated token>"
+url = "https://api.github.com/user/repos?access_token=" + access_token
 
+response = None
 try:
-    response = requests.post(url, repo_info)
-    if response.status_code != 200:
+    response = requests.post(url, json=repo_info)
+    if response.status_code != 201:
         show_error_and_exit(response.text)
 except requests.exceptions.RequestException as e:
     show_error_and_exit(e)
 
-print(response)
+response_json = response.json()
+print(response_json["name"] + " created successfully")
 init_git = input('Init git project & connect to remote (y or n)?')
 if init_git.strip().lower() == "y":
-    print("Init Git")
+    clone_url = response_json['clone_url']
+    print(clone_url)
 
 print("Thank You !")
